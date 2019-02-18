1642f2b4f2c950d23e649a82c363804ddc90c987
Cameron Bost <cameron.a.bost@gmail.com>
2019-02-18 15:06:44 -0600

    Created AStarGraph.java, mostly pushing mcgovern updates

---
+++ src/spacesettlers/bost7517/AStarGraph.java
@@ -0,0 +1,20 @@
+package spacesettlers.bost7517;
+
+public class AStarGraph {
+	class Vertex{
+		int x, y;
+	}
+	
+	/**
+	 * Discretizes the simulation into a grid
+	 * 
+	 * @param _height
+	 * @param _width
+	 * @param asteroidRadius
+	 */
+	AStarGraph(int _height, int _width, int asteroidRadius){
+		int graphWidth = _width / asteroidRadius, graphHeight = _height / asteroidRadius;
+		
+	}
+
+}

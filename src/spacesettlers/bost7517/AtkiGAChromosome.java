bec5f656b47f903c3f181d7fd48706c3a765b5a2
Josh Atkinson <Joshua.j.Atkinson-1@ou.edu>
2019-03-31 09:23:20 -0500

    ATKI uses GA method

---
+++ src/spacesettlers/bost7517/AtkiGAChromosome.java
@@ -0,0 +1,314 @@
+package spacesettlers.bost7517;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+import spacesettlers.clients.TeamClient;
+import spacesettlers.actions.AbstractAction;
+import spacesettlers.actions.DoNothingAction;
+import spacesettlers.actions.MoveToObjectAction;
+import spacesettlers.actions.PurchaseCosts;
+import spacesettlers.actions.PurchaseTypes;
+import spacesettlers.graphics.LineGraphics;
+import spacesettlers.graphics.StarGraphics;
+import spacesettlers.objects.AbstractActionableObject;
+import spacesettlers.objects.AbstractObject;
+import spacesettlers.objects.Asteroid;
+import spacesettlers.objects.Base;
+import spacesettlers.objects.Ship;
+import spacesettlers.objects.resources.ResourcePile;
+import spacesettlers.simulator.Toroidal2DPhysics;
+import spacesettlers.utilities.Position;
+
+/**
+ * An example chromosome for a space settlers agent using genetic algorithms / evolutionary computation
+ * 
+ * @author amy
+ *
+ */
+public class AtkiGAChromosome {
+	private HashMap<AtkiGAState, AbstractAction> policy;
+	HashMap <UUID, Ship> asteroidToShipMap;
+	private int timeSincePlan = 10;
+	private AStarPath currentPath = null;
+	private spacesettlers.bost7517.AStarGraph graph;
+	private LinkedList<AStarPath> currentSearchTree;
+	private LinkedList<Position> pointsToVisit;
+	Toroidal2DPhysics space;
+	boolean debug = false;
+	HashMap <UUID, Boolean> aimingForBase;
+	HashMap <UUID, Boolean> justHitBase;
+	
+	/**
+	 * Final Variables
+	 */
+	final double LOW_ENERGY_THRESHOLD = 2750; // #P1 - Lowered 2000 -> 1500
+	final double RESOURCE_THRESHOLD = 1000;   // #P1 - Raised 500 -> 2000
+	final double BASE_BUYING_DISTANCE = 400; // #P1 - raised 200 -> 350 
+	
+	
+	public AtkiGAChromosome()
+	{
+		
+		policy = new HashMap<AtkiGAState, AbstractAction>();
+		asteroidToShipMap = new HashMap<UUID, Ship>();
+		graph = new spacesettlers.bost7517.AStarGraph(1000, 1000, false);
+		aimingForBase = new HashMap<UUID, Boolean>();
+		justHitBase = new HashMap<UUID, Boolean>();
+	}
+
+	/**
+	 * Returns either the action currently specified by the policy or randomly selects one if this is a new state
+	 * 
+	 * @param currentState
+	 * @return
+	 */
+	public AbstractAction getCurrentAction(Toroidal2DPhysics space, Ship myShip, AtkiGAState currentState, int policyNumber) 
+	{
+		drawSearchTree(space);
+		if (!policy.containsKey(currentState)) {
+			/*
+			 ###################
+			 ## Original code ##
+			 ###################
+			 
+			 
+			// randomly chose to either do nothing or go to the nearest
+			// asteroid.  Note this needs to be changed in a real agent as it won't learn 
+			// much here!
+			
+			
+			if (rand.nextBoolean()) {
+				policy.put(currentState, new DoNothingAction());
+			} else {
+				//System.out.println("Moving to nearestMineable Asteroid " + myShip.getPosition() + " nearest " + currentState.getNearestMineableAsteroid().getPosition());
+				policy.put(currentState, new MoveToObjectAction(space, myShip.getPosition(), currentState.getNearestMineableAsteroid()));
+			}
+			
+			
+			  ##############
+			  ## NEW CODE ##
+			  ##############
+			  
+			 */
+			AbstractAction current = myShip.getCurrentAction();
+			
+			if(policyNumber == 1)
+			{
+				if(current != null)
+				{
+					if(currentState.getEnergySource() != null) {
+						//Re-plans every 20 timesteps.
+						if(timeSincePlan >= 10) {
+						current = null; //resets current stepp to null so it is able to update step
+							timeSincePlan = 0; // resets times plan back to 0
+							currentPath = graph.getPathTo(myShip,  currentState.getEnergySource(), space); //Wil get the current path that a* has chosen
+							currentSearchTree = graph.getSearchTree(); //Returns a search tree 
+							pointsToVisit = new LinkedList<Position>(currentPath.getPositions()); // Will contain all the points for a*
+						}
+						else
+						{
+							timeSincePlan++;
+						}
+						
+						if(current != null && (myShip.getEnergy() > LOW_ENERGY_THRESHOLD)) // Want to make sure not to interrupt an a* move that has not finished yet.
+						{
+							if(!current.isMovementFinished(space)) //checks if the object has stopped moving
+							{
+								return current;
+							}
+						}
+						// Call points to create a new action to move to that object.
+						if (pointsToVisit != null)
+						{
+							if(!pointsToVisit.isEmpty())
+							{	
+								Position newPosition = new Position(pointsToVisit.getFirst().getX(),pointsToVisit.getFirst().getY());
+								policy.put(currentState, new BDSMMoveAction(space, myShip.getPosition(), newPosition));
+								//Will display graphics if set to true.
+								/*if(showMyGraphics)
+								{
+									//LINE!!!
+										graphicsToAdd.add(new StarGraphics(3, Color.RED, newPosition));
+										LineGraphics line = new LineGraphics(currentPosition, newPosition, 
+												space.findShortestDistanceVector(currentPosition, newPosition));
+										line.setLineColor(Color.RED);
+										graphicsToAdd.add(line);
+									}*/
+									
+								pointsToVisit.poll();//pops the top
+							}
+						}
+							
+						//Planning currently need to run local search
+						policy.put(currentState, new BDSMMoveToObjectAction(space, myShip.getPosition(), currentState.getEnergySource()));			
+					}
+				}
+			}
+			
+			if(policyNumber == 2)
+			{
+				if(timeSincePlan >= 10) {
+					current = null;
+					timeSincePlan = 0;
+					if(currentState.getEnergySource()!=null)
+						currentPath = graph.getPathTo(myShip,  currentState.getEnergySource(), space);
+					currentSearchTree = graph.getSearchTree();
+					pointsToVisit = new LinkedList<Position>(currentPath.getPositions());
+				}
+				else
+				{
+					timeSincePlan++;
+				}
+				
+				//Checks that the previous A* action is completed.
+				if(current != null)
+				{
+					if(!current.isMovementFinished(space))
+					{
+						aimingForBase.put(myShip.getId(), true);
+						return current;
+					}
+				}
+				if (pointsToVisit != null)
+				{
+					if(!pointsToVisit.isEmpty())
+					{	
+						Position newPosition = new Position(pointsToVisit.getFirst().getX(),pointsToVisit.getFirst().getY());
+						policy.put(currentState, new BDSMMoveAction(space, myShip.getPosition(), newPosition));
+						//This will display graphics if they are enabled.
+						/*
+						 * if(showMyGraphics)
+						{
+						//LINE!!!
+							graphicsToAdd.add(new StarGraphics(3, Color.RED, newPosition));
+							LineGraphics line = new LineGraphics(currentPosition, newPosition, 
+									space.findShortestDistanceVector(currentPosition, newPosition));
+							line.setLineColor(Color.RED);
+							graphicsToAdd.add(line);
+						}
+						*/
+						pointsToVisit.poll();//pops the top
+						aimingForBase.put(myShip.getId(), true);
+					}
+				}
+				
+				//Run a Local search for base
+				policy.put(currentState, new BDSMMoveToObjectAction(space, myShip.getPosition(), currentState.getBase()));	
+				aimingForBase.put(myShip.getId(), true);
+				
+				if(debug){
+					System.out.println("<Action Declaration> - Deposit (" + myShip.getResources().getTotal()+")");
+				}
+			}
+			if(policyNumber == 3)
+			{
+				if (currentState.nearestMineableAsteroid != null) {
+					System.out.println("<DEBUG> - calling policy inside of policy 3");
+					asteroidToShipMap.put(currentState.nearestMineableAsteroid.getId(), myShip);
+					//Re-plans every 10 steps.
+					if(timeSincePlan >= 10) {
+						current = null;
+						timeSincePlan = 0;
+						currentPath = graph.getPathTo(myShip,  currentState.nearestMineableAsteroid, space);
+						currentSearchTree = graph.getSearchTree();
+						pointsToVisit = new LinkedList<Position>(currentPath.getPositions());
+					}
+					else
+					{
+						timeSincePlan++;
+					}
+					//Checks to make sure that the current A* is move is finished.
+					if(current != null)
+					{
+						if(!current.isMovementFinished(space))
+						{
+							return current;
+						}
+					}
+					System.out.println("HERE");
+					//Will create actions for A* points.
+					if (pointsToVisit != null)
+					{
+						if(!pointsToVisit.isEmpty())
+						{	
+							//Will assign a Position variable with the positions.
+							Position newPosition = new Position(pointsToVisit.getFirst().getX(),pointsToVisit.getFirst().getY());
+							//Create the action to move to the A* position.
+							//newAction = new BDSMMoveAction(space, myShip.getPosition(), newPosition, currentState.nearestMineableAsteroid);
+							policy.put(currentState, new BDSMMoveAction(space, myShip.getPosition(), newPosition, currentState.nearestMineableAsteroid));
+							//This will displayed graphics if true.
+							/*if(showMyGraphics)
+							{
+								graphicsToAdd.add(new StarGraphics(3, Color.RED, newPosition));
+								LineGraphics line = new LineGraphics(currentPosition, newPosition, 
+										space.findShortestDistanceVector(currentPosition, newPosition));
+								line.setLineColor(Color.RED);
+								graphicsToAdd.add(line);
+							}*/
+							
+							pointsToVisit.poll();//pops the top
+						}
+					}
+					
+					policy.put(currentState, new BDSMMoveToObjectAction(space, myShip.getPosition(), currentState.nearestMineableAsteroid,currentState.nearestMineableAsteroid.getPosition().getTranslationalVelocity()));
+					}
+				}
+			}
+		return policy.get(currentState);
+		}
+	
+	
+	void drawSearchTree(Toroidal2DPhysics space) {
+		if(currentSearchTree != null) {
+			HashSet<LineGraphics> drawnPositions = new HashSet<>();
+			for(AStarPath path: currentSearchTree) {
+				Position prevPosition = null;
+				for(Position p: path.getPositions()) {
+					if(prevPosition != null) {
+						LineGraphics nextLine = new LineGraphics(prevPosition, p, space.findShortestDistanceVector(prevPosition, p));
+						drawnPositions.add(nextLine);
+					}
+					prevPosition = p;
+				}
+			}
+			for(LineGraphics g: drawnPositions) {
+				//graphicsToAdd.add(g);
+			}
+		}
+		
+		if(currentPath != null) {
+			HashSet<LineGraphics> drawnPositions = new HashSet<>();
+
+			Position prevPosition = null;
+			for(Position p: currentPath.getPositions()) {
+				if(prevPosition != null) {
+					LineGraphics nextLine = new LineGraphics(prevPosition, p, space.findShortestDistanceVector(prevPosition, p));
+					drawnPositions.add(nextLine);
+				}
+				prevPosition = p;
+			}
+			for(LineGraphics g: drawnPositions) {
+				g.setLineColor(Color.GREEN);
+				//graphicsToAdd.add(g);
+			}
+			
+		}
+	}
+	
+	
+	
+	/**
+	 * Update time step for plan.
+	 */
+	public void currentPolicyUpdateTime() 
+	{
+		timeSincePlan = timeSincePlan + 1;
+	}
+}
---
+++ src/spacesettlers/bost7517/AtkiGAClient.java
@@ -0,0 +1,265 @@
+package spacesettlers.bost7517;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.XStreamException;
+
+import spacesettlers.actions.AbstractAction;
+import spacesettlers.actions.DoNothingAction;
+import spacesettlers.actions.PurchaseCosts;
+import spacesettlers.actions.PurchaseTypes;
+import spacesettlers.clients.TeamClient;
+import spacesettlers.graphics.SpacewarGraphics;
+import spacesettlers.objects.AbstractActionableObject;
+import spacesettlers.objects.AbstractObject;
+import spacesettlers.objects.Base;
+import spacesettlers.objects.Ship;
+import spacesettlers.objects.powerups.SpaceSettlersPowerupEnum;
+import spacesettlers.objects.resources.ResourcePile;
+import spacesettlers.simulator.Toroidal2DPhysics;
+import spacesettlers.utilities.Position;
+
+/**
+ * Demonstrates one idea on implementing Genetic Algorithms/Evolutionary Computation within the space settlers framework.
+ * 
+ * @author amy
+ *
+ */
+
+public class AtkiGAClient extends TeamClient {
+	/**
+	 * The current policy for the team
+	 */
+	private AtkiGAChromosome currentPolicy;
+	
+	/**
+	 * The current population (either being built or being evaluated)
+	 */
+	private AtkiGAPopulation population;
+	
+	/**
+	 * How many steps each policy is evaluated for before moving to the next one
+	 */
+	private int evaluationSteps = 2000;
+	
+	/**
+	 * How large of a population to evaluate
+	 */
+	private int populationSize = 25;
+	
+	/**
+	 * Current step
+	 */
+	private int steps = 0;
+	/**
+	 * AStar
+	 */
+	private LinkedList<Position> pointsToVisit;
+	private AStarPath currentPath = null;
+	private LinkedList<AStarPath> currentSearchTree;
+	private spacesettlers.bost7517.AStarGraph graph;
+	
+	/**
+	 * Final Variables
+	 */
+	final double LOW_ENERGY_THRESHOLD = 4750; // #P1 - Lowered 2000 -> 1500
+	final double RESOURCE_THRESHOLD = 2000;   // #P1 - Raised 500 -> 2000
+	final double BASE_BUYING_DISTANCE = 400; // #P1 - raised 200 -> 350 
+	
+	@Override
+	public Map<UUID, AbstractAction> getMovementStart(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects) {
+		// make a hash map of actions to return
+		HashMap<UUID, AbstractAction> actions = new HashMap<UUID, AbstractAction>();
+
+		// loop through each ship and send it the current policy from the chromosome.  If the chromosome
+		// hasn't seen a state before, it will pick an abstract action (you should make more interesting actions!
+		// this agent chooses only between doNothing and moveToNearestAsteroid)
+		for (AbstractObject actionable :  actionableObjects) {
+			if (actionable instanceof Ship) {
+				Ship ship = (Ship) actionable;
+
+				AbstractAction action = null;
+				
+				//Will choose policy 1. to collect energy shield
+				
+				if ((ship.getEnergy() < LOW_ENERGY_THRESHOLD)) 
+				{
+					AtkiGAState currentState = new AtkiGAState(space, ship);	
+					action = currentPolicy.getCurrentAction(space, ship, currentState, 1);				
+				}
+				
+				//Will choose policy 2. to return to base.
+				
+				
+				if (ship.getResources().getTotal() > RESOURCE_THRESHOLD) 
+				{
+					AtkiGAState currentState = new AtkiGAState(space, ship);	
+					action = currentPolicy.getCurrentAction(space, ship, currentState, 2);	
+					
+				}
+				
+				//Will choose policy 3. to collect NEAREST resource.
+				if (ship.getCurrentAction() == null || ship.getCurrentAction().isMovementFinished(space))
+				{		
+					AtkiGAState currentState = new AtkiGAState(space, ship);			
+					action = currentPolicy.getCurrentAction(space, ship, currentState, 3);
+				} 		
+
+				actions.put(ship.getId(), action);
+			} 
+			/*else {
+				// it is a base.  Heuristically decide when to use the shield (TODO)
+				actions.put(actionable.getId(), new DoNothingAction());
+			}*/
+		}
+		System.out.println("actions are " + actions);
+		return actions;
+
+	}
+
+	@Override
+	public void getMovementEnd(Toroidal2DPhysics space, Set<AbstractActionableObject> actionableObjects) {
+		// increment the step counter
+		steps++;
+
+		// if the step counter is modulo evaluationSteps, then evaluate this member and move to the next one
+		if (steps % evaluationSteps == 0) {
+			// note that this method currently scores every policy as zero as this is part of 
+			// what the student has to do
+			population.evaluateFitnessForCurrentMember(space);
+
+			// move to the next member of the population
+			currentPolicy = population.getNextMember();
+
+			if (population.isGenerationFinished()) {
+				// note that this is also an empty method that a student needs to fill in
+				population.makeNextGeneration();
+				
+				currentPolicy = population.getNextMember();
+			}
+			
+		}
+		
+	}
+
+	@Override
+	public Map<UUID, SpaceSettlersPowerupEnum> getPowerups(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	/**
+	 * If there is enough resourcesAvailable, buy a base.  Place it by finding a ship that is sufficiently
+	 * far away from the existing bases
+	 */
+	public Map<UUID, PurchaseTypes> getTeamPurchases(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects, 
+			ResourcePile resourcesAvailable, 
+			PurchaseCosts purchaseCosts) {
+
+		HashMap<UUID, PurchaseTypes> purchases = new HashMap<UUID, PurchaseTypes>();
+		
+		// TODO: Determine if base is impossible to purchase, add heuristic to purchase anyway
+		boolean bought_base = false;
+		
+		if (purchaseCosts.canAfford(PurchaseTypes.BASE, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				if (actionableObject instanceof Ship) {
+					Ship ship = (Ship) actionableObject;
+					Set<Base> bases = space.getBases();
+
+					// how far away is this ship to a base of my team?
+					boolean buyBase = true;
+					for (Base base : bases) {
+						if (base.getTeamName().equalsIgnoreCase(getTeamName())) {
+							double distance = space.findShortestDistance(ship.getPosition(), base.getPosition());
+							if (distance < BASE_BUYING_DISTANCE) {
+								buyBase = false;
+							}
+						}
+					}
+					if (buyBase) {
+						purchases.put(ship.getId(), PurchaseTypes.BASE);
+						bought_base = true;
+						System.out.println("Buying a base!!");
+						break;
+					}
+				}
+			}		
+		} 
+		
+		// Ship Purchase
+		if (bought_base == false && purchaseCosts.canAfford(PurchaseTypes.SHIP, resourcesAvailable)) {
+			for (AbstractActionableObject actionableObject : actionableObjects) {
+				// TODO: What if we chose a base to spawn our ship at for a reason, instead of the first available one?
+				if (actionableObject instanceof Base) {
+					Base base = (Base) actionableObject;
+					purchases.put(base.getId(), PurchaseTypes.SHIP);
+					break;
+				}
+			}
+		}
+		return purchases;
+	}
+
+	/**
+	 * Initialize the population by either reading it from the file or making a new one from scratch
+	 * 
+	 * @param space
+	 */
+	@Override
+	public void initialize(Toroidal2DPhysics space) {
+		XStream xstream = new XStream();
+		xstream.alias("ExampleGAPopulation", AtkiGAPopulation.class);
+
+		// try to load the population from the existing saved file.  If that failes, start from scratch
+		try { 
+			population = (AtkiGAPopulation) xstream.fromXML(new File(getKnowledgeFile()));
+		} catch (XStreamException e) {
+			// if you get an error, handle it other than a null pointer because
+			// the error will happen the first time you run
+			System.out.println("No existing population found - starting a new one from scratch");
+			population = new AtkiGAPopulation(populationSize);
+		}
+		currentPolicy = population.getFirstMember();
+	}
+
+	@Override
+	public void shutDown(Toroidal2DPhysics space) {
+		XStream xstream = new XStream();
+		xstream.alias("ExampleGAPopulation", AtkiGAPopulation.class);
+
+		try { 
+			// if you want to compress the file, change FileOuputStream to a GZIPOutputStream
+			xstream.toXML(population, new FileOutputStream(new File(getKnowledgeFile())));
+		} catch (XStreamException e) {
+			// if you get an error, handle it somehow as it means your knowledge didn't save
+			System.out.println("Can't save knowledge file in shutdown ");
+			System.out.println(e.getMessage());
+		} catch (FileNotFoundException e) {
+			// file is missing so start from scratch (but tell the user)
+			System.out.println("Can't save knowledge file in shutdown ");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public Set<SpacewarGraphics> getGraphics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
